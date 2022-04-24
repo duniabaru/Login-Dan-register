1f804cf33c21aab4e97c4abc846fdb89a9b883db
Murah Senyum <48380727+duniabaru@users.noreply.github.com>
2022-04-24 15:34:31 +0700

    Add files via upload

---
+++ Login dan register/latihan.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Ini Php</title>
+</head>
+
+<body>
+<?php
+echo'Selamat datang';
+
+      //strlen()
+      // str_word_count()
+      // strrev()
+      // strpos()
+      // str_replace(find,replace,string)
+      // str_repeat(teks,jumlah)
+?>
+</body>
+
+</html>
---
+++ Login dan register/welcome.php
@@ -0,0 +1,23 @@
+<?php
+
+session_start();
+
+if (!isset($_SESSION['username'])) {
+    header("Location: index.php");
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome</title>
+</head>
+<body>
+    <?php echo "<h1>Welcome " . $_SESSION['username'] . "</h1>"; ?>
+    <H1>Selamat kamu berhasil Login</H1>
+    <a href="logout.php">Logout</a>
+</body>
+</html>
